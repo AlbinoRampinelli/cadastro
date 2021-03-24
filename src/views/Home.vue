6c6009632c47707064db46c6499bb9a0cf20b8bb
Albino Rampinelli <albino@dds.com.br>
2021-03-24 12:42:12 -0300

    primeiro projeto

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+      <h1>Bem-vindo!</h1>
+      <button class = "btn btn-danger" @click="efetuarLogout">Sair</button> 
+  </div>
+</template>
+
+<script>
+import { logoutMixin } from '@/mixins'
+export default {
+  name: 'home',
+  mixins: [ logoutMixin ]
+}
+</script>
